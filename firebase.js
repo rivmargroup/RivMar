68daf75c2c44d93d732f8c837e6b7a7d7d4f82f8
rivmargroup <rivmargroup@gmail.com>
2025-05-07 20:09:15 -0400

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,33 @@
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_ID",
+  appId: "YOUR_APP_ID"
+};
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+
+function login() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  auth.signInWithEmailAndPassword(email, password)
+    .then(() => {
+      alert("Logged in!");
+      window.location.href = "index.html";
+    })
+    .catch(err => alert("Login failed: " + err.message));
+}
+
+function register() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  auth.createUserWithEmailAndPassword(email, password)
+    .then(() => {
+      alert("Registered successfully!");
+      window.location.href = "index.html";
+    })
+    .catch(err => alert("Signup failed: " + err.message));
+}
