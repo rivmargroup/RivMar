c120366f9810c61e1f8aa75ead3def1a8f569ba1
rivmargroup <rivmargroup@gmail.com>
2025-05-08 21:42:22 -0400

    Update file.js

--- file.js
+++ file.js
@@ -15,23 +15,29 @@ const storage = firebase.storage();
 auth.onAuthStateChanged(user => {
   if (user) {
     const uid = user.uid;
-    const userFolder = `clientes/${uid}/`; // estructura de carpetas por usuario
+    const userFolder = `users_${uid}/`; // correcta estructura de carpeta
 
     const storageRef = storage.ref(userFolder);
 
     storageRef.listAll().then((res) => {
       const fileList = document.getElementById("file-list");
+
       res.items.forEach((itemRef) => {
         itemRef.getDownloadURL().then(url => {
           const ext = itemRef.name.split('.').pop().toLowerCase();
           const isVideo = ['mp4', 'mov', 'webm'].includes(ext);
+
+          const container = document.createElement('div');
+          container.classList.add('file-item');
 
           const element = document.createElement(isVideo ? 'video' : 'img');
           element.src = url;
           element.controls = isVideo;
           element.width = 300;
           element.style.margin = '10px';
-          fileList.appendChild(element);
+
+          container.appendChild(element);
+          fileList.appendChild(container);
         });
       });
     }).catch(error => {
